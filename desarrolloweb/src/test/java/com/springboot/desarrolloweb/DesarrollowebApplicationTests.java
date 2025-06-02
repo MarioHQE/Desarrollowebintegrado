ef8857cb21a6ccae122010f806bee6f399afcec4
MarioHQE <166763888+MarioHQE@users.noreply.github.com>
2025-06-02 13:09:18 -0500

    Implementacion de errores y implementacion test container dinamicos con docker

--- desarrolloweb/src/test/java/com/springboot/desarrolloweb/DesarrollowebApplicationTests.java
+++ desarrolloweb/src/test/java/com/springboot/desarrolloweb/DesarrollowebApplicationTests.java
@@ -1,10 +1,37 @@
 package com.springboot.desarrolloweb;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.containers.MySQLContainer;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DesarrollowebApplicationTests {
+
+	@SuppressWarnings("resource")
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+			.withDatabaseName("testdb")
+			.withUsername("testuser")
+			.withPassword("testpass");
+
+	static {
+		mysql.start();
+	}
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mysql::getJdbcUrl);
+		registry.add("spring.datasource.username", mysql::getUsername);
+		registry.add("spring.datasource.password", mysql::getPassword);
+		registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+		registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQLDialect");
+	}
 
 	@Test
 	void contextLoads() {
