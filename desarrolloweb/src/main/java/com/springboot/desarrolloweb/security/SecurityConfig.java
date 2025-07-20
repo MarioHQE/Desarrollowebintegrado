13639de31d020110624492b1869f4fe4e92a31bb
MarioHQE <166763888+MarioHQE@users.noreply.github.com>
2025-07-19 21:42:02 -0500

    cambios nuevos

--- desarrolloweb/src/main/java/com/springboot/desarrolloweb/security/SecurityConfig.java
+++ desarrolloweb/src/main/java/com/springboot/desarrolloweb/security/SecurityConfig.java
@@ -160,7 +160,6 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.addAllowedOrigin("http://localhost:3600"); // Agregar el puerto de tu app
                 configuration.addAllowedOrigin("http://localhost:5173");
                 configuration.addAllowedOrigin("http://localhost:8080");
                 configuration.addAllowedOrigin("http://127.0.0.1:*"); // Para Stripe CLI
